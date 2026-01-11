88ff54a13341e8f698d1cd69e701f183998b8b12
lovewanshik0511 <kamleshl@triazinesoft.com>
2026-01-11 12:52:10 +0530

    add site-map

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.rknextgen.com',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    // optional
+    // robotsTxtOptions: {
+    //   additionalSitemaps: [
+    //     'https://www.rknextgen.com/server-sitemap.xml', // <==== Add this
+    //   ],
+    // },
+}
